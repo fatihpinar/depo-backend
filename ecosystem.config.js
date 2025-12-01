5aea7b08125adb6c54ac557e0c0f8b62d0b6cd95
Fatih Pınar <fatihpinar@outlook.com>
2025-12-01 23:54:55 +0300

    Add pm2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "depo-backend",
+      script: "src/server.js", // senin server giriş dosyan
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
